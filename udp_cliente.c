59452f91ae89d0179660cad50574733ad0186ec9
João Pedro <joaopedrolaguardia@gmail.com>
2024-11-29 17:22:47 -0300

    primeira versão

---
+++ udp_cliente.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 5000
+#define BUFFER_SIZE 1024
+#define FILE_NAME "arquivo_recebido.bin"
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    socklen_t server_len;
+    char buffer[BUFFER_SIZE];
+    int packets_rec = 0;
+    size_t total_bytes_received = 0;
+    FILE *file;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+
+    const char *message = "Enviar arquivo";
+    sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    file = fopen(FILE_NAME, "wb");
+
+    clock_t start_time = clock();
+
+    while (1) {
+        ssize_t bytes_received = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server_addr, &server_len);
+        if (bytes_received > 0 && strncmp(buffer, "FIM", 3) == 0) {
+            printf("Arquivo transferido com sucesso.\n");
+            break;
+        }
+        fwrite(buffer, 1, bytes_received, file);
+        packets_rec++; 
+        if (bytes_received < BUFFER_SIZE) {
+            break;
+        }
+    }
+
+    clock_t end_time = clock();
+    double total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    double download_speed = total_bytes_received / total_time;
+
+    printf("Arquivo recebido com sucesso!\n");
+    fclose(file);
+    close(sockfd);
+    return 0;
+}
---
+++ udp_servidor.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 5000
+#define BUFFER_SIZE 1024
+#define FILE_NAME "arquivo.bin"
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len;
+    char buffer[BUFFER_SIZE];
+    int packets_sent = 0;
+    FILE *file;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Erro ao criar socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    printf("Servidor aguardando na porta %d...\n", PORT);
+
+    client_len = sizeof(client_addr);
+    memset(buffer, 0, BUFFER_SIZE);
+
+    recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len);
+    printf("Solicitação recebida de %s:%d\n",
+           inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    file = fopen(FILE_NAME, "rb");
+
+    while (!feof(file)) {
+        size_t bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
+        if (bytes_read > 0) {
+            sendto(sockfd, buffer, bytes_read, 0, (struct sockaddr *)&client_addr, client_len);
+            packets_sent++;
+        }
+    }
+
+    const char *end_message = "FIM";
+    sendto(sockfd, end_message, strlen(end_message), 0, (const struct sockaddr *)&client_addr, client_len);
+
+    printf("Arquivo enviado com sucesso!\n");
+    fclose(file);
+    close(sockfd);
+    return 0;
+}
